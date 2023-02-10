b6d7226f1febb2cdb2ff664e3581bccb5563ef4f
Jaggoseut <zhenyavolkov1995@yandex.ru>
2023-02-10 14:03:03 +0300

    Фикс метода zipFiles() в Main.java для арихивирования всех файлов, но не могу понять чего не хватает

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,48 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
 
-    static void saveGame(String s) {
-        GameProgress gameProgress = new GameProgress(20, 2, 50, 954.50);
-        GameProgress gameProgress1 = new GameProgress(80, 3, 55, 600.30);
-        GameProgress gameProgress2 = new GameProgress(50, 5, 65, 600.30);
+    static void saveGame(String s, GameProgress gameProgress) {
         try (FileOutputStream fos = new FileOutputStream(s, true); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(gameProgress);
-            oos.writeObject(gameProgress1);
-            oos.writeObject(gameProgress2);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
     }
 
-    static void zipFiles(String s) {
-        try (ZipOutputStream zip = new ZipOutputStream(new FileOutputStream("F:/Game/savegames/zip.zip", true)); FileInputStream fis = new FileInputStream(s)){
-            ZipEntry entry = new ZipEntry("zipSave.dat");
-            zip.putNextEntry(entry);
-            byte [] buffer = new byte[fis.available()];
-            fis.read(buffer);
-            zip.write(buffer);
-            zip.closeEntry();
+    static void zipFiles(String s, List<String> listSave) {
+//        try (ZipOutputStream zip = new ZipOutputStream(new FileOutputStream("F:/Game/savegames/zip.zip", true)); FileInputStream fis = new FileInputStream(s)){
+//            ZipEntry entry = new ZipEntry("zipSave.dat");
+//            zip.putNextEntry(entry);
+//            byte [] buffer = new byte[fis.available()];
+//            fis.read(buffer);
+//            zip.write(buffer);
+//            zip.closeEntry();
+//        } catch (Exception ex) {
+//            System.out.println(ex.getMessage());
+
+ //       }
+        try (ZipOutputStream zip = new ZipOutputStream(new FileOutputStream("F:/Game/savegames/save.zip"))){
+
+            for(String file  : listSave) {
+                File fileToZip = new File(file);
+                FileInputStream fis = new FileInputStream(fileToZip);
+                zip.putNextEntry(new ZipEntry(fileToZip.getName()));
+                int length;
+
+                byte[] b = new byte[2048];
+
+                while((length = fis.read(b)) > 0) {
+                    zip.write(b, 0, length);
+                }
+                fis.close();
+                zip.close();
+            }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
@@ -70,8 +87,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        saveGame("F:/Game/savegames/save.dat");
-        zipFiles("F:/Game/savegames/save.dat");
+        List<String> listSave = Arrays.asList("F:/Game/savegames/save1.dat", "F:/Game/savegames/save2.dat", "F:/Game/savegames/save3.dat");
+        saveGame(listSave.get(0), new GameProgress(20, 2, 50, 954.50));
+        saveGame(listSave.get(1), new GameProgress(80, 3, 55, 600.30));
+        saveGame(listSave.get(2), new GameProgress(50, 5, 65, 600.30));
+        zipFiles("F:/Game/savegames/zip.zip", listSave);
         delete("F:/Game/savegames/save.dat");
         zipFilesRevert("F:/Game/savegames/zip.zip");
         desirealize("F:/Game/savegames/newSave.dat");
